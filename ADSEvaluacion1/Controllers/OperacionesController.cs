79d6573a1465c8050ccab1ef7722fc3eac683ca6
gerardoJoseVilledaErazo <gerardovilleda@gmail.com>
2021-03-13 11:17:42 -0600

    version 1.03

--- ADSEvaluacion1/Controllers/OperacionesController.cs
+++ ADSEvaluacion1/Controllers/OperacionesController.cs
@@ -12,20 +12,41 @@ namespace ADSEvaluacion1.Controllers
         // GET: Operaciones/Operaciones
         public ActionResult Operaciones()
         {
-            Operaciones op = new Operaciones() { op1 = 11, op2 = 3, suma = 0};
+            Operaciones op = new Operaciones() { op1 =11, op2 = 3, suma = 0};
             op.suma = op.op1 + op.op2;
             op.resta = op.op1 - op.op2;
             op.mult = op.op1 * op.op2;
             op.div = op.op1 / op.op2;
-            /*
-            String opc = "";
-            switch (opc)
-            {
-                case "Suma": op.suma = op.op1 + op.op2; break;
-                case "Resta": op.resta = op.op1 - op.op2; break;
-                case "Multiplicacion": op.mult = op.op1 * op.op2; break;
-                case "Division": op.div = op.op1 / op.op2; break;
-            }*/
+            return View(op);
+        }
+
+
+        public ActionResult Suma()
+        {
+            Operaciones op = new Operaciones() { op1 = 11, op2 = 3, suma = 0 };
+            op.suma = op.op1 + op.op2;
+
+            return View(op);
+        }
+
+        public ActionResult Resta()
+        {
+            Operaciones op = new Operaciones() { op1 = 11, op2 = 3, resta = 0 };
+            op.resta = op.op1 - op.op2;
+            return View(op);
+        }
+
+        public ActionResult Multiplicacion()
+        {
+            Operaciones op = new Operaciones() { op1 = 11, op2 = 3, mult = 0 };
+            op.mult = op.op1 * op.op2;
+            return View(op);
+        }
+
+        public ActionResult Division()
+        {
+            Operaciones op = new Operaciones() { op1 = 11, op2 = 3, div = 0 };
+            op.div = op.op1 / op.op2;
             return View(op);
         }
     }
