175686f7ec3a88500902a212eba2bdeb7147653d
gerardoJoseVilledaErazo <gerardovilleda@gmail.com>
2021-03-13 10:55:24 -0600

    version 1.02

--- ADSEvaluacion1/Controllers/OperacionesController.cs
+++ ADSEvaluacion1/Controllers/OperacionesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ADSEvaluacion1.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,24 @@ namespace ADSEvaluacion1.Controllers
 {
     public class OperacionesController : Controller
     {
-        // GET: Operaciones/
-        public ActionResult Index()
+        // GET: Operaciones/Operaciones
+        public ActionResult Operaciones()
         {
-            return View();
+            Operaciones op = new Operaciones() { op1 = 11, op2 = 3, suma = 0};
+            op.suma = op.op1 + op.op2;
+            op.resta = op.op1 - op.op2;
+            op.mult = op.op1 * op.op2;
+            op.div = op.op1 / op.op2;
+            /*
+            String opc = "";
+            switch (opc)
+            {
+                case "Suma": op.suma = op.op1 + op.op2; break;
+                case "Resta": op.resta = op.op1 - op.op2; break;
+                case "Multiplicacion": op.mult = op.op1 * op.op2; break;
+                case "Division": op.div = op.op1 / op.op2; break;
+            }*/
+            return View(op);
         }
     }
 }
---
+++ ADSEvaluacion1/Models/Operaciones.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ADSEvaluacion1.Models
+{
+    public class Operaciones
+    {
+        //Atributos
+        public double op1 {get; set; }
+        public double op2 { get; set; }
+        public double suma { get; set; }        
+        public double resta { get; set; }
+        public double mult { get; set; }
+        public double div { get; set; }
+    }
+}
